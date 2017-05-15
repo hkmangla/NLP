07b40e06e78046041d9aa4f8f62e277cfe99aaee
hkmangla <hemantmangla78@gmail.com>
2017-05-15 20:29:30 +0530

    Implement Parser Model

--- assignment2/q2_initialization.py
+++ assignment2/q2_initialization.py
@@ -25,6 +25,9 @@ def xavier_weight_init():
             out: tf.Tensor of specified shape sampled from the Xavier distribution.
         """
         ### YOUR CODE HERE
+        shape = tuple(shape)
+        epsilon = tf.sqrt(6.0 / sum(shape))
+        out = tf.Variable(tf.random_uniform(list(shape),-epsilon,epsilon))
         ### END YOUR CODE
         return out
     # Returns defined initializer function.
--- assignment2/q2_parser_model.py
+++ assignment2/q2_parser_model.py
@@ -54,6 +54,9 @@ class ParserModel(Model):
         (Don't change the variable names)
         """
         ### YOUR CODE HERE
+        self.input_placeholder = tf.placeholder(tf.int32,[None,self.config.n_features])
+        self.labels_placeholder = tf.placeholder(tf.float32,[None,self.config.n_classes])
+        self.dropout_placeholder = tf.placeholder(tf.float32)
         ### END YOUR CODE
 
     def create_feed_dict(self, inputs_batch, labels_batch=None, dropout=1):
@@ -79,6 +82,12 @@ class ParserModel(Model):
             feed_dict: The feed dictionary mapping from placeholders to values.
         """
         ### YOUR CODE HERE
+        feed_dict = {
+            self.input_placeholder : inputs_batch,
+            self.dropout_placeholder : dropout
+        }
+        if labels_batch is not None:
+            feed_dict[self.labels_placeholder] = labels_batch
         ### END YOUR CODE
         return feed_dict
 
@@ -100,6 +109,10 @@ class ParserModel(Model):
             embeddings: tf.Tensor of shape (None, n_features*embed_size)
         """
         ### YOUR CODE HERE
+        emb = tf.Variable(self.pretrained_embeddings)
+        lookup_emb = tf.nn.embedding_lookup(emb,self.input_placeholder)
+        
+        embeddings = tf.reshape(lookup_emb,[-1,self.config.n_features*self.config.embed_size])
         ### END YOUR CODE
         return embeddings
 
@@ -130,6 +143,16 @@ class ParserModel(Model):
 
         x = self.add_embedding()
         ### YOUR CODE HERE
+        initializer = xavier_weight_init()
+        with tf.variable_scope("parse") as pp:
+            W = initializer((self.config.n_features*self.config.embed_size,self.config.hidden_size))
+            b1 = tf.Variable(tf.zeros([self.config.hidden_size,], tf.float32))
+            U = initializer((self.config.hidden_size,self.config.n_classes))
+            b2 = tf.Variable(tf.zeros([self.config.n_classes,], tf.float32))
+            h = tf.nn.relu(tf.matmul(x,W) + b1)
+            h_drop = tf.nn.dropout(h,self.dropout_placeholder)
+            pred = tf.matmul(h_drop,U) + b2
+
         ### END YOUR CODE
         return pred
 
@@ -147,6 +170,8 @@ class ParserModel(Model):
             loss: A 0-d tensor (scalar)
         """
         ### YOUR CODE HERE
+        cross_entropy_loss = tf.nn.softmax_cross_entropy_with_logits(logits = pred,labels = self.labels_placeholder)
+        loss = tf.reduce_mean(cross_entropy_loss)
         ### END YOUR CODE
         return loss
 
@@ -170,6 +195,7 @@ class ParserModel(Model):
             train_op: The Op for training.
         """
         ### YOUR CODE HERE
+        train_op = tf.train.AdamOptimizer(learning_rate=self.config.lr).minimize(loss)
         ### END YOUR CODE
         return train_op
 
@@ -208,7 +234,7 @@ class ParserModel(Model):
         self.build()
 
 
-def main(debug=True):
+def main(debug=False):
     print 80 * "="
     print "INITIALIZING"
     print 80 * "="
@@ -224,10 +250,10 @@ def main(debug=True):
         parser.model = model
         print "took {:.2f} seconds\n".format(time.time() - start)
 
-        init = tf.global_variables_initializer()
+        # init = tf.global_variables_initializer()
         # If you are using an old version of TensorFlow, you may have to use
         # this initializer instead.
-        # init = tf.initialize_all_variables()
+        init = tf.initialize_all_variables()
         saver = None if debug else tf.train.Saver()
 
         with tf.Session() as session:
